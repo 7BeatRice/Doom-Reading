412ccef07dfd0748c9cf64591ba3e3b0237d1b99
Beatrice Olaosebikan <bto334@cs.utexas.edu>
2026-06-09 15:19:41 -0500

    set up express server to listen at a port

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+const app = express()
+
+app.use('./public', express.static('./public'))
+app.use('/scripts', express.static('./public/scripts'))
+
+//route for root url
+app.get('/', (req, res) => {
+    res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">Doom-Reading API</h1>')
+}
+)
+
+//starts a server on a port
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () =>{
+    console.log(`Server lisitening on http://localhost:${PORT}`)
+})
